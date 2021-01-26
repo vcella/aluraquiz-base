55a6c8f8210f8628ab4c24e010fbafcd78bfee86
Vladimir Cella <cella.vladimir@gmail.com>
2021-01-26 18:21:56 -0300

    Segunda correção

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import db from '../db.json'
 import Widget from '../src/components/Widget'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubcorner'
+import GitHubCorner from '../src/components/GitHubCorner'
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
